d50311adce23c914bc06ad1284fabfa6ea753bb0
Jessica Marais <jessica_marais@hotmail.com>
2019-10-09 15:16:09 +0100

    Update to .rb file

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -29,7 +29,7 @@ class Bookmark
       connection = PG.connect(dbname: 'bookmark_manager')
     end
 
-    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, url, title")
+    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, url, title;")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
